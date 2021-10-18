da988f2f4cfe8e139f90a3fc113ca54079892911
Gabriel Baruque <baruque@edu.unirio.br>
2021-10-18 19:48:31 -0300

    Adicionar arquivos de projeto.

---
+++ Model/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppLocadora.wwwroot.Model
+{
+    public class Movie
+    {
+        //ID - Identificador único para nossos objetos
+
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
