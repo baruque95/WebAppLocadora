356cdfb91c8677476b710cb55373a96b8a196793
Gabriel Baruque <baruque@edu.unirio.br>
2021-10-18 22:11:34 -0300

    Funções básicas do Framework ASP.Net Sistema simples de cadastro de filmes.

---
+++ Data/WebAppLocadoraContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppLocadora.wwwroot.Model;
+
+namespace WebAppLocadora.Data
+{
+    public class WebAppLocadoraContext : DbContext
+    {
+        public WebAppLocadoraContext (DbContextOptions<WebAppLocadoraContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppLocadora.wwwroot.Model.Movie> Movie { get; set; }
+    }
+}
